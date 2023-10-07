9e13c80a5ad7b72236eb64c67e8d583dc2e43d3f
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 18:57:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ from pages import home, login, register
 
 def main():
     st.title("LitWave")
-    menu = st.sidebar.selectbox("Menú", ["Inicio", "Entrar", "Registrarse"])
-
-    if menu == "Inicio":
-        home.show()
-
-    elif menu == "Entrar":
-        login.show()
-
-    elif menu == "Registrarse":
-        register.show()
 
 if __name__ == "__main__":
     main()
