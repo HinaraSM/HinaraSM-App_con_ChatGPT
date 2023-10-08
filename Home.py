e4b313734a7ec5ab8e75dcb37e5f85d2a55ca6d8
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 22:55:45 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -52,3 +52,20 @@ st.markdown("---")
 if not resultados:
     st.info("No se encontraron resultados para la búsqueda.")
 
+
+# Logo en la esquina superior derecha
+st.markdown(
+    """
+    <style>
+    .logo-container {
+        display: flex;
+        justify-content: flex-end;
+        padding: 10px;
+    }
+    </style>
+    <div class="logo-container">
+        <img src="https://images.vexels.com/media/users/3/229082/isolated/preview/6fabc24c3830d75486725cc6d786dfbb-logotipo-de-circulos-de-libro.png" alt="Logo" style="width:100px;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
