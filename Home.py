c0540b8593d3ecd85fe4ed19e2ac46b934809523
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 19:02:49 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,6 +3,16 @@ from pages import home, login, register
 
 
 st.title("LitWave")
+st.header("Bienvenido a LitWave")
+st.write("LitWave es una plataforma para los amantes de la lectura, \
+donde puedes descubrir nuevos libros, compartir tus experiencias \
+de lectura y conectarte con otros lectores.")
+st.markdown("### Características Destacadas:")
+st.write("- Descubre nuevos libros recomendados para ti.")
+st.write("- Conecta con otros lectores y comparte tus recomendaciones.")
+# ... (resto del código de la página de inicio)
+
+
 # Información de contacto y redes sociales en el pie de página
 st.markdown("---")
 st.markdown("### Contacto")
