f1f5901398450413ca2a26e39181dd691831c983
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 22:28:22 -0500

    Update Registrarse.py

--- pages/Registrarse.py
+++ pages/Registrarse.py
@@ -1,4 +1,26 @@
 import streamlit as st
 
-st.header("Registro")
-# ... (resto del código de la página de registro)
+st.title("Regístrate en LitWave")
+
+# Campos de entrada para el registro
+nombres_apellidos = st.text_input("Nombres y Apellidos")
+usuario = st.text_input("Usuario")
+contrasena = st.text_input("Contraseña", type="password")
+confirmar_contrasena = st.text_input("Confirmar Contraseña", type="password")
+correo_electronico = st.text_input("Correo Electrónico")
+fecha_nacimiento = st.date_input("Fecha de Nacimiento")
+
+# Casillas de verificación para términos y condiciones, y política de privacidad
+aceptar_terminos = st.checkbox("Al marcar esta casilla, acepto los términos y condiciones del servicio.")
+aceptar_privacidad = st.checkbox("Al marcar esta casilla, confirmo que he leído y aceptado la política de privacidad de la aplicación.")
+
+# Botón para registrar al usuario
+if st.button("Registrarse"):
+    # Verificar si las contraseñas coinciden y las casillas de verificación están marcadas
+    if contrasena == confirmar_contrasena and aceptar_terminos and aceptar_privacidad:
+        # Guardar el usuario en la base de datos o realizar otras acciones necesarias
+        st.success("Registro exitoso. ¡Bienvenido, {}!".format(nombres_apellidos))
+    elif contrasena != confirmar_contrasena:
+        st.error("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.")
+    else:
+        st.error("Debes aceptar los términos y condiciones y la política de privacidad para registrarte.")
