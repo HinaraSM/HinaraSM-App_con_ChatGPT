0c570b096875855ba795ea34af8eae262d1a7cdf
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 21:39:27 -0500

    Update Entrar.py

--- pages/Entrar.py
+++ pages/Entrar.py
@@ -1,3 +1,29 @@
 import streamlit as st
 
 st.header("Inicio de Sesión")
+
+# Datos del usuario de prueba
+usuario_prueba = {
+    "usuario": "hinara",
+    "contrasena": "hinara12"
+}
+
+# Para hacer pruebas, puedes almacenar el usuario de prueba en una lista o un diccionario en memoria
+usuarios_registrados = [usuario_prueba]
+
+# Simular la autenticación del usuario
+usuario_input = input("Ingrese su nombre de usuario: ")
+contrasena_input = input("Ingrese su contraseña: ")
+
+# Verificar las credenciales del usuario
+usuario_autenticado = None
+for usuario in usuarios_registrados:
+    if usuario["usuario"] == usuario_input and usuario["contrasena"] == contrasena_input:
+        usuario_autenticado = usuario
+        break
+
+if usuario_autenticado:
+    print("Inicio de sesión exitoso.")
+else:
+    print("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
+
