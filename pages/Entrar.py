dd8822f3d1a17c91677de3949aaeb76a15ae65ec
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 21:51:35 -0500

    Update Entrar.py

--- pages/Entrar.py
+++ pages/Entrar.py
@@ -1,29 +1,24 @@
 import streamlit as st
 
-st.header("Inicio de Sesión")
-
 # Datos del usuario de prueba
 usuario_prueba = {
-    "usuario": "hinara",
-    "contrasena": "hinara12"
+    "usuario": "usuario_de_prueba",
+    "contrasena": "contrasena_de_prueba"
 }
 
-# Para hacer pruebas, puedes almacenar el usuario de prueba en una lista o un diccionario en memoria
-usuarios_registrados = [usuario_prueba]
+# Título de la aplicación
+st.title("Aplicación de Autenticación de Usuarios")
 
-# Simular la autenticación del usuario
-usuario_input = input("Ingrese su nombre de usuario: ")
-contrasena_input = input("Ingrese su contraseña: ")
+# Casilla de entrada para el nombre de usuario
+usuario_input = st.text_input("Ingrese su nombre de usuario")
 
-# Verificar las credenciales del usuario
-usuario_autenticado = None
-for usuario in usuarios_registrados:
-    if usuario["usuario"] == usuario_input and usuario["contrasena"] == contrasena_input:
-        usuario_autenticado = usuario
-        break
+# Casilla de entrada para la contraseña
+contrasena_input = st.text_input("Ingrese su contraseña", type="password")
 
-if usuario_autenticado:
-    print("Inicio de sesión exitoso.")
-else:
-    print("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
-
+# Botón para iniciar sesión
+if st.button("Iniciar Sesión"):
+    # Verificar las credenciales del usuario
+    if usuario_input == usuario_prueba["usuario"] and contrasena_input == usuario_prueba["contrasena"]:
+        st.success("Inicio de sesión exitoso.")
+    else:
+        st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
