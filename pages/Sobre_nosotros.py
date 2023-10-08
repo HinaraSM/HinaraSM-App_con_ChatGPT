ed4f998ab78e4c04220cc105f172930796ff3750
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-07 21:48:47 -0500

    Update Sobre_nosotros.py

--- pages/Sobre_nosotros.py
+++ pages/Sobre_nosotros.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 st.title("LitWave")
 st.markdown("## Bienvenido a la red social de los lectores")
-st.markdown("LitWave es una plataforma para los amantes de la lectura, donde podrás:")
+st.markdown("### LitWave es una plataforma para los amantes de la lectura, donde podrás:")
 st.write("- Descubrir nuevos libros recomendados especialmente para ti.")
 st.write("- Conectar, interactuar y compartir tu pasión por los libros.")
 st.write("- Comentar tus opiniones sobre libros y autores.")
