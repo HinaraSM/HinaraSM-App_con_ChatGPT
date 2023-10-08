783c8abd3c20e0bd4cc2e061c87c5c04828ef128
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-08 00:58:56 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.markdown(
     }
     </style>
     <div class="logo-container">
-        <img src="https://i.ibb.co/D9Hq01h/logo.png" alt="logo">
+        <img src="https://i.ibb.co/CWhPGm1/logo.png" alt="logo">
     </div>
     """,
     unsafe_allow_html=True
