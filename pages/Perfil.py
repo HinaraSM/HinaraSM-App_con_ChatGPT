4eb70f5501b59ac7222043cf22e9e049fd6eb236
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-22 18:37:42 -0500

    Update Perfil.py

--- pages/Perfil.py
+++ pages/Perfil.py
@@ -40,7 +40,7 @@ if 'usuario' in st.experimental_get_query_params():
 
         # Botón para cerrar sesión
         if st.button("Cerrar Sesión"):
-            session_state.is_authenticated = False
+            st.experimental_set_query_params(usuario=session_state.usuario[""])
             st.success("Sesión cerrada exitosamente. ¡Hasta luego!")
 
     else:
