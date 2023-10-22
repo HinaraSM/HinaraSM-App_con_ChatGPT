e702d3ef58fe4f69596b722eb30f59a0dba06f84
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2023-10-22 14:30:19 -0500

    Update Entrar.py

--- pages/Entrar.py
+++ pages/Entrar.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from pymongo import MongoClient
+from datetime import datetime
+
+# Conexión a la base de datos MongoDB
+#client = MongoClient("mongodb://tu_usuario:tu_contraseña@tu_host:tu_puerto/tu_base_de_datos")
+#db = client.tu_base_de_datos
+#usuarios_collection = db.usuarios
+
+# Datos del usuario de prueba
+usuario_prueba = {
+    "usuario": "hinara",
+    "contrasena": "hinara12"
+}
+
+# Función para verificar las credenciales del usuario en la base de datos
+def verificar_credenciales(usuario_input, contrasena_input):
+    usuario_encontrado = usuario_prueba.find_one({"usuario": usuario_input, "contrasena": contrasena_input})
+    return usuario_encontrado
+
+# Estado de la aplicación
+is_authenticated = False
 
 # Logo en la esquina superior derecha
 st.markdown(
@@ -11,31 +32,42 @@ st.markdown(
     }
     </style>
     <div class="logo-container">
-        <img src="https://i.ibb.co/CWhPGm1/logo.png" alt="logo"style="max-width: 150px; height: auto;">
+        <img src="https://i.ibb.co/CWhPGm1/logo.png" alt="logo" style="max-width: 150px; height: auto;">
     </div>
     """,
     unsafe_allow_html=True
 )
 
-# Título de la aplicación
-st.title("Inicia sesión en LitWave")
+if not is_authenticated:
+    st.title("Inicia sesión en LitWave")
 
-# Datos del usuario de prueba
-usuario_prueba = {
-    "usuario": "hinara",
-    "contrasena": "hinara12"
-}
+    usuario_input = st.text_input("Ingrese su nombre de usuario")
+    contrasena_input = st.text_input("Ingrese su contraseña", type="password")
 
-# Casilla de entrada para el nombre de usuario
-usuario_input = st.text_input("Ingrese su nombre de usuario")
+    if st.button("Iniciar Sesión"):
+        usuario = verificar_credenciales(usuario_input, contrasena_input)
+        if usuario:
+            is_authenticated = True
+            st.success("Inicio de sesión exitoso. ¡Bienvenido, {}!".format(usuario["nombres_apellidos"]))
+        else:
+            st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
+else:
+    st.title("¡Bienvenido a LitWave!")
 
-# Casilla de entrada para la contraseña
-contrasena_input = st.text_input("Ingrese su contraseña", type="password")
+    # Opciones para usuarios autenticados
+    # Puedes agregar aquí las secciones específicas para los usuarios logueados.
+    # Por ejemplo:
+    # st.write("¡Hola, usuario! Aquí están las opciones disponibles para ti:")
 
-# Botón para iniciar sesión
-if st.button("Iniciar Sesión"):
-    # Verificar las credenciales del usuario
-    if usuario_input == usuario_prueba["usuario"] and contrasena_input == usuario_prueba["contrasena"]:
-        st.success("Inicio de sesión exitoso.")
-    else:
-        st.error("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.")
+    # Sección 1
+    st.header("Mis Tareas")
+    # ... Aquí puedes agregar el contenido de la sección de tareas ...
+
+    # Sección 2
+    st.header("Perfil")
+    # ... Aquí puedes agregar el contenido de la sección de perfil ...
+
+    # Sección 3
+    st.header("Configuración")
+    # ... Aquí puedes agregar el contenido de la sección de configuración ...
+
